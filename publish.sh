a51c090edd010e5acdd326a1c0c977c437f8113d
nobilissimum <ronn.angelo.lee@gmail.com>
2024-06-14 18:12:05 +0800

    Remove unnecessary shell script

--- publish.sh
+++
@@ -1,9 +0,0 @@
-echo "Compiling theme..."
-echo $(time source .venv/bin/activate)
-echo $(time python main.py build)
-echo $(time python main.py release)
-
-echo "\n"
-
-echo "Creating .vsix file..."
-echo $(time bunx vsce package)
